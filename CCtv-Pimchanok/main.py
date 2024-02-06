10e83ef60595a4e97b5a24f0d5510f0cf4714c01
Pimchanok <1166109060853@mail.rmutt.ac.th>
2024-02-06 17:42:09 +0700

    บลอร์ภาพ

--- CCtv-Pimchanok/main.py
+++ CCtv-Pimchanok/main.py
@@ -3,23 +3,23 @@ import cv2 as cv
 import requests
 
 # เปิดกล้องเว็บแคมที่ติดตั้งในเครื่อง (ใช้หมายเลข 0)
-camera = cv.VideoCapture(0)
+camera = cv.VideoCapture(0)      #จำนวนกล้องที่มี
 
-# กำหนด URL ของ LINE Notify API และ Token ของตัวเอง
-url = 'https://notify-api.line.me/api/notify'
+
+url = 'https://notify-api.line.me/api/notify'     # Url ของ Line และ token
 token = 'YyC8BVO6omqWQ9SaE3O3SsTehmQvWBKxdC0mLrrkquB'
 
-# กำหนดหัวข้อเสริม (headers) สำหรับ HTTP request ไปยัง LINE Notify
+# กำหนดหัวข้อเสริม (headers) ไปยัง LINE Notify
 headers = {
     'content-type': 'application/x-www-form-urlencoded',
     'Authorization': 'Bearer ' + token
 }
 
-# ข้อความจะถูกส่งไปยัง LINE Notify เมื่อมีการตรวจพบการเคลื่อนไหว
-msg = 'has movement'
 
-# เริ่มลูปคลาวด์เอาเปิดกล้องและตรวจสอบภาพ
-while camera.isOpened():
+msg = 'ตรวจพบความเคลื่อนไหว'  #กำหนดข้อความ
+
+
+while camera.isOpened():    #คำสั่งเปิดกล้อง
     # อ่านภาพจากกล้องเว็บแคมสองครั้ง
     retry, screen1 = camera.read()
     retry, screen2 = camera.read()
@@ -31,7 +31,7 @@ while camera.isOpened():
     gray = cv.cvtColor(difference, cv.COLOR_RGB2GRAY)
 
     # ทำการบลอร์ภาพสีเทา
-    blur = cv.GaussianBlur(gray, (5, 5), 0)
+    blur = cv.GaussianBlur(gray, (8, 8), 0)
 
     # กำหนดค่าสิ้นสุด (threshold) เพื่อสร้างภาพขาวดำที่มีการเคลื่อนไหว
     _, threshold = cv.threshold(blur, 20, 255, cv.THRESH_BINARY)
@@ -39,25 +39,26 @@ while camera.isOpened():
     # ขยายขนาดพื้นที่ที่เคลื่อนไหวด้วยการขยายขนาด (dilation)
     dilation = cv.dilate(threshold, None, iterations=5)
 
-    # ค้นหาขอบ (contours) ในภาพที่ถูกขยายขนาด
+   # cv.drawContours(screen1, contours , -1,(0,255,0),2)
     contours, _, = cv.findContours(dilation, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     # วาดขอบรอบการเคลื่อนไหวบนภาพต้นฉบับ
     cv.drawContours(screen1, contours, -1, (0, 255, 0), 2)
     
     # ตรวจสอบและส่งข้อความผ่าน LINE Notify เมื่อตรวจพบการเคลื่อนไหวของภาพ
-    for movement in contours:
-        if cv.contourArea(movement) < 8000:
+    for movement in contours:                   #คำสั่งตรวจจับตลอดเวลา
+        if cv.contourArea(movement) < 8000:     # ตรวจจับความเคลื่อนไหว
             continue
         x, y, height, width = cv.boundingRect(movement)
-        cv.rectangle(screen1, (x, y), (x+height, y+width), (0, 255, 0), 2)
+        cv.rectangle(screen1, (x, y), (x+height, y+width), (0, 288, 0), 4)    
 
         notify = requests.post(url, headers=headers, data={'message': msg})
         print(notify.text)
         
     # รอรับข้อมูลและทำการแสดงภาพบนหน้าต่างที่แสดงบน "pyCCTV"
-    if cv.waitKey(10) == ord('q'):
+    if cv.waitKey(10) == ord('q'):    # คำสั่งปิดกล้อง
         break
     
     cv.imshow('pyCCTV', screen1)
-
+    
+    #สิ้นสุดคำสั่ง และคำสั่งจะลูปไปเรื่อยๆจนกว่าจะทำการปิดกล้อง
